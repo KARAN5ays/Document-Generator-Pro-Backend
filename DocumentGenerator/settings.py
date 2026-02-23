0299394383c90262bf6454ba8abfe1016f31941a
Karan Pokhrel <karanpokhrel999@gmail.com>
2026-02-23 10:42:35 +0545

    fixed template issues

--- DocumentGenerator/settings.py
+++ DocumentGenerator/settings.py
@@ -64,8 +64,8 @@ MEDIA_ROOT = BASE_DIR / 'media' #this is the path where the generated pdf will b
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- backendapp/views/document_types.py
+++ backendapp/views/document_types.py
@@ -21,11 +21,11 @@ class DocumentTypeListView(APIView):
 
     def get(self, request):
         types = DocumentType.objects.filter(Q(created_by=request.user) | Q(created_by__isnull=True))
-        serializer = DocumentTypeSerializer(types, many=True)
+        serializer = DocumentTypeSerializer(types, many=True, context={'request': request})
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = DocumentTypeSerializer(data=request.data)
+        serializer = DocumentTypeSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save(created_by=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -46,7 +46,7 @@ class DocumentTypeDetailView(APIView):
             doc_type = DocumentType.objects.get(pk=pk)
             if doc_type.created_by and doc_type.created_by != request.user:
                  return Response({"detail": "You do not have permission to view this template."}, status=status.HTTP_403_FORBIDDEN)
-            serializer = DocumentTypeSerializer(doc_type)
+            serializer = DocumentTypeSerializer(doc_type, context={'request': request})
             return Response(serializer.data)
         except DocumentType.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
