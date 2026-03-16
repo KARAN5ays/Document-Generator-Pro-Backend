ecc65fc941fb282eddc9bdc5970bf1615ba0aba3
Karan Pokhrel <karanpokhrel999@gmail.com>
2026-03-16 11:53:38 +0545

    changed appname to documentapp

--- DocumentGenerator/settings.py
+++ DocumentGenerator/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
-    "backendapp",
+    "documentapp",
 ]
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
